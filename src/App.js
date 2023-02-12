39ce110ab8403d77cc507194771263358cc4973f
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-02-12 00:20:59 -0600

    make adjustments for home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Home from './home';
-import Q1 from './Quiz1';
+// import Q1 from './Quiz1';
 
 function App() {
   return (
     <>
     <Home />
-    <Q1 />
+    {/* <Q1 /> */}
     </>
   );
 }
